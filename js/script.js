8338ed22d358215205f8b2c293bff4a8973f5e41
stella <floraaaaaa1130@gmail.com>
2026-01-25 01:50:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ const npcs = {
     },
     ryo: { 
         name: "료", 
-        sprite: "assets/images/sprites/ryp.png",
+        sprite: "assets/images/sprites/ryo.png",
         portraits: { 
             default: "assets/images/portraits/ryo_default.png",
             happy: "assets/images/portraits/ryo_happy.png",
@@ -197,15 +197,15 @@ const itemData = {
     "커피": { img: "assets/images/items/coffee.png", desc: "고소한 원두 향이 난다" }, //ㅎㅇ
     "블루 재즈": { img: "assets/images/items/bluejazz.png", desc: "동그란 모양의 푸른 꽃" }, //ㅎㅇ
     "치즈": { img: "assets/images/items/cheese.png", desc: "꼬릿꼬릿한 냄새가 난다" }, //ㅎㅇ
-    "설탕": { img: "assets/images/items/sugar.png", desc: "혈당 관리 해야하는데..." }, //ㅎㅇ
+    "설탕": { img: "assets/images/items/Sugar.png", desc: "혈당 관리 해야하는데..." }, //ㅎㅇ
     "핑크케이크": { img: "assets/images/items/pink_cake.png", desc: "사랑스러운 핑크색 케이크" }, //ㅎㅇ
-    "초코케이크": { img: "assets/images/items/chocolate_cake.png", desc: "찐한 초콜릿 냄새가 난다" }, //ㅎㅇ
-    "딸기": { img: "assets/images/items/strawberry.png", desc: "뚜왈기!" }, //ㅎㅇ
+    "초코케이크": { img: "assets/images/items/chocolatecake.png", desc: "찐한 초콜릿 냄새가 난다" }, //ㅎㅇ
+    "딸기": { img: "assets/images/items/Strawberry.png", desc: "뚜왈기!" }, //ㅎㅇ
     "행운의 점심": { img: "assets/images/items/luckylunch.png", desc: "행운이 올 것 같다!" }, //ㅎㅇ
-    "밀가루": { img: "assets/images/items/flour.png", desc: "제빵의 기본 재료" }, //ㅎㅇ
+    "밀가루": { img: "assets/images/items/Flour.png", desc: "제빵의 기본 재료" }, //ㅎㅇ
     "달걀": { img: "assets/images/items/egg.png", desc: "작고 소중한 달걀" }, //ㅎㅇ
-    "흙": { img: "assets/images/items/clay.png", desc: "흙이다" }, //ㅎㅇ
-    "스타푸르트": { img: "assets/images/items/starfruit.png", desc: "별 모양의 과일" }, //ㅎㅇ
+    "흙": { img: "assets/images/items/Clay.png", desc: "흙이다" }, //ㅎㅇ
+    "스타푸르트": { img: "assets/images/items/Starfruit.png", desc: "별 모양의 과일" }, //ㅎㅇ
     "스타드롭커피": { img: "assets/images/items/stardropcoffee.png", desc: "커피가 상큼할 수 있다니" }, //ㅎㅇ
     "리모컨": { img: "assets/images/items/remote.png", desc: "음...?" }, //ㅎㅇ
     "꽃다발": { img: "assets/images/items/bouquet.png", desc: "받으면 행복할 것 같다" }, //ㅎㅇ
@@ -213,3 +213,4 @@ const itemData = {
     "도토리": { img: "assets/images/items/acorn.png", desc: "다람쥐가 좋아할 것 같다" }, //ㅎㅇ
     "에너지 드링크": { img: "assets/images/items/energytonic.png", desc: "피로가 싹 가신다" } //ㅎㅇ
 };
+
